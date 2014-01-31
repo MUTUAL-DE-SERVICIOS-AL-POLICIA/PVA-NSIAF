6744f19cdacf202bb3de7f74ad91b0a37c766c09
Carol Silva <csilva@geo.gob.bo>
2014-01-31 16:54:04 -0400

    changes in user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,6 +69,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:code, :name, :post, :ci, :email, :password, :phone, :cellular)
+      params.require(:user).permit(:code, :name, :post, :ci, :email, :password, :phone, :mobile)
     end
 end
