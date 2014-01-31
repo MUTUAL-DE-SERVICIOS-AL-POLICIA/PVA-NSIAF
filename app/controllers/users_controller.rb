615f12e845fbbdbae074ff771016f418dfebea12
Elmer Mendoza <emendoza@geo.gob.bo>
2014-01-31 16:11:26 -0400

    Save records in database from DBF file to users table

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -72,8 +72,8 @@ class UsersController < ApplicationController
   # POST /users/import
   # Importa los datos del archivo DBF dentro de la tabla usuarios.
   def import
-    User.import_dbf(params[:dbf])
-    redirect_to :back, notice: 'No hay lio'
+    inserted, nils = User.import_dbf(params[:dbf])
+    redirect_to :back, notice: "#{inserted + nils} total registros. #{inserted} registros insertados. #{nils} registros nulos."
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,23 +4,59 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates :username, presence: true, uniqueness: true
+
+  CORRELATIONS = {
+    'CODRESP' => 'code',
+    'NOMRESP' => 'name',
+    'CARGO' => 'title',
+    #'CODOFIC' => 'department_id',
+    'API_ESTADO' => 'status'
+  }
+
   ##
   # Importar el archivo DBF a la tabla de usuarios
   def self.import_dbf(dbf)
+    # TODO falta asignar a un departamento
     users = DBF::Table.new(dbf.tempfile)
-    users.each_with_index do |record, index|
-      print "#{index}.-\t"
-      if record.present?
-        print "#{record['USUAR'].inspect} ,"
-        print "#{record['CODRESP'].inspect} ,"
-        print "#{record['NOMRESP'].inspect} ,"
-        print "#{record['CARGO'].inspect} ,"
-        print "#{record['CODOFIC'].inspect} ,"
-        print "#{record['API_ESTADO'].inspect} ,"
-      else
-        print "nil"
+    i = 0; j = 0
+    transaction do
+      users.each_with_index do |record, index|
+        print "#{index}.-\t"
+        if record.present?
+          CORRELATIONS.each { |k, v| print "#{record[k].inspect}, " }
+          token = get_unique_tokensss
+          save_correlations(record, token)
+          i += 1
+        else
+          j += 1
+          print record.inspect
+        end
+        puts ''
       end
-      puts ''
+    end
+    [i, j]
+  end
+
+  private
+
+  ##
+  # Guarda en la base de datos de acuerdo a la correspondencia de campos.
+  def self.save_correlations(record, token)
+    user = Hash.new
+    CORRELATIONS.each do |origin, destination|
+      user.merge!({ destination => record[origin] })
+    end
+    user.merge!({ 'username' => token })
+    user.merge!({ 'email' => "#{token}@test.com" })
+    user.merge!({ 'password' => token })
+    create!(user)
+  end
+
+  def self.get_unique_tokensss
+    loop do
+      token = SecureRandom.urlsafe_base64(7)
+      break token unless User.exists?(username: token)
     end
   end
 end
