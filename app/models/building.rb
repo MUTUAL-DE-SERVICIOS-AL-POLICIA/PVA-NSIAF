443e0048a2c9382c3244e7a95fe24b2a3ca96f78
Elmer Mendoza <emendoza@geo.gob.bo>
2014-02-03 10:58:24 -0400

    Import Building DBF file to MySql Database

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,4 +1,12 @@
 class Building < ActiveRecord::Base
+  include ImportDbf
+
+  CORRELATIONS = {
+    'UNIDAD' => 'code',
+    'DESCRIP' => 'name',
+    'ENTIDAD' => 'entity_id'
+  }
+
   belongs_to :entity
 
   validates :name, :entity_id, presence: true
@@ -6,5 +14,18 @@ class Building < ActiveRecord::Base
 
   def entity_name
     entity.present? ? entity.name : ''
+  end
+
+  private
+
+  ##
+  # Guarda en la base de datos de acuerdo a la correspondencia de campos.
+  def self.save_correlations(record)
+    CORRELATIONS.each { |k, v| print "#{record[k].inspect}, " }
+    building = Hash.new
+    CORRELATIONS.each do |origin, destination|
+      building.merge!({ destination => record[origin] })
+    end
+    new(building).save(validate: false) if building.present?
   end
 end
