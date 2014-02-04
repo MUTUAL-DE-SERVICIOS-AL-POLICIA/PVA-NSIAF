a9d2f018f1e51de51900808b450b61c52adac219
Elmer Mendoza <emendoza@geo.gob.bo>
2014-02-04 16:12:32 -0400

    Import DBF asset file to database

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,9 +1,18 @@
 class Asset < ActiveRecord::Base
+  include ImportDbf
+
+  CORRELATIONS = {
+    'CODIGO' => 'code',
+    'DESCRIP' => 'description',
+    'CODAUX' => 'auxiliary_id',
+    'CODRESP' => 'user_id'
+  }
+
   belongs_to :auxiliary
   belongs_to :user
 
   validates :code, presence: true, uniqueness: true
-  validates :description, presence: true
+  validates :description, :auxiliary_id, :user_id, presence: true
 
   def auxiliary_name
     auxiliary.present? ? auxiliary.name : ''
