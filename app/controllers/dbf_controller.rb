273c85e0c756daabc356651f00b4763f8100ca38
Elmer Mendoza <emendoza@geo.gob.bo>
2014-02-06 10:32:58 -0400

    Display file names for import DBF

--- app/controllers/dbf_controller.rb
+++ app/controllers/dbf_controller.rb
@@ -8,12 +8,22 @@ class DbfController < ApplicationController
   # POST /dbf/:model/import
   # Importa los datos del archivo DBF dentro de la tabla usuarios.
   def import
-    if params[:dbf].present?
+    if check_dbf_file(params[:dbf])
       klass = params[:model].classify.safe_constantize
       inserted, nils = klass.import_dbf(params[:dbf])
       redirect_to :back, notice: "#{inserted + nils} total registros. #{inserted} registros insertados. #{nils} registros nulos."
     else
-      redirect_to :back, alert: "Especifique un archivo para importar"
+      redirect_to :back, alert: "Especifique el archivo <b>#{view_context.get_filename(params[:model])}</b> para migrar"
     end
+  end
+
+  private
+
+  def check_dbf_file(dbf)
+    dbf.present? && is_dbf_file?(dbf)
+  end
+
+  def is_dbf_file?(dbf)
+    view_context.dbf_mime_types.include?(dbf.content_type) && view_context.get_filename(params[:model]) == dbf.original_filename
   end
 end
