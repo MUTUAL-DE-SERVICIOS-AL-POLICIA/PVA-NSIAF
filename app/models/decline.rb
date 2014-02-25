3da26a44d3161dd629dd2c99cdbee9184c342fe3
Carol Silva <csilva@geo.gob.bo>
2014-02-25 15:06:51 -0400

    add new decline

--- app/models/decline.rb
+++ app/models/decline.rb
@@ -10,4 +10,13 @@ class Decline < ActiveRecord::Base
   def user_name
     user ? user.name : ''
   end
+
+  def self.deregister(asset, description, reason, user_id)
+    auxiliary = asset.auxiliary
+    account = auxiliary.account
+    userc = asset.user
+    department = userc.department
+    decline = new(asset_code: asset.code, account_code: account.code, auxiliary_code: auxiliary.code, department_code: department.code, user_code: userc.code, description: description, reason: reason, user_id: user_id)
+    decline.save!
+  end
 end
