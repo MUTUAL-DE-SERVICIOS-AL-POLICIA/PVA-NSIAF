479145810ca5b51efa80446e92c5ba62e3fe19dc
Carol Silva <csilva@geo.gob.bo>
2014-06-24 18:11:50 -0400

    changes in show article

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,12 @@ class Article < ActiveRecord::Base
   include ManageStatus
 
   belongs_to :material
+
+  def material_code
+    material.present? ? material.code : ''
+  end
+
+  def material_name
+    material.present? ? material.description : ''
+  end
 end
