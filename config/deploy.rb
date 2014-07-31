f4a85a48ec7230108b2fe253c679e0d824dc4d55
Elmer Mendoza <emendoza@geo.gob.bo>
2014-07-31 11:00:52 -0400

    Create linked dir for public/uploads

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,7 +29,7 @@ set :log_level, :debug
 set :linked_files, %w{config/database.yml config/secrets.yml}
 
 # Default value for linked_dirs is []
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle}
+set :linked_dirs, %w{bin log public/uploads tmp/pids tmp/cache tmp/sockets vendor/bundle}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
