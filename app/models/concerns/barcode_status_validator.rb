918e8ab2a15583c3a7bfbbd65a13a762c5165bb0
Elmer Mendoza <emendoza@geo.gob.bo>
2014-08-01 18:45:58 -0400

    Fix barcode validator class when save same barcode

--- app/models/concerns/barcode_status_validator.rb
+++ app/models/concerns/barcode_status_validator.rb
@@ -5,8 +5,10 @@ class BarcodeStatusValidator
 
   def validate
     barcode = Barcode.find_by_code @barcode_object.barcode
-    if !barcode.present? || !barcode.is_free?
-      @barcode_object.errors.add(:barcode, "no existe o ya está en uso")
+    if @barcode_object.barcode != @barcode_object.barcode_was
+      if !barcode.present? || !barcode.is_free?
+        @barcode_object.errors.add(:barcode, "no existe o ya está en uso")
+      end
     end
   end
 end
