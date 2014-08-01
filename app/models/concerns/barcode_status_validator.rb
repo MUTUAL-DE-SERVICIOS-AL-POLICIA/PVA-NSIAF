a53faea6ad78eda18eb25f249322e899de1c355b
Elmer Mendoza <emendoza@geo.gob.bo>
2014-08-01 15:42:11 -0400

    Display validation/error message for barcode field

--- app/models/concerns/barcode_status_validator.rb
+++ app/models/concerns/barcode_status_validator.rb
@@ -6,7 +6,7 @@ class BarcodeStatusValidator
   def validate
     barcode = Barcode.find_by_code @asset.barcode
     if !barcode.present? || !barcode.is_free?
-      @asset.errors[:base] << "Este Código de Barras no existe o ya está en uso"
+      @asset.errors.add(:barcode, "no existe o ya está en uso")
     end
   end
 end
