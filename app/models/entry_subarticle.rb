e5dd09e8eca1965a25b97f2d389b925117d0161f
Carol Silva <csilva@geo.gob.bo>
2014-11-21 15:20:48 -0400

    add show note_entry

--- app/models/entry_subarticle.rb
+++ app/models/entry_subarticle.rb
@@ -4,4 +4,16 @@ class EntrySubarticle < ActiveRecord::Base
 
   #validates :amount, :invoice, presence: true, numericality: { only_integer: true, greater_than: 0 }
   #validates :unit_cost, :total_cost, presence: true, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }, numericality: { greater_than: 0, less_than: 10000000 }
+
+  def subarticle_name
+    subarticle.present? ? subarticle.description : ''
+  end
+
+  def subarticle_code
+    subarticle.present? ? subarticle.code : ''
+  end
+
+  def subarticle_unit
+    subarticle.present? ? subarticle.unit : ''
+  end
 end
--- app/models/note_entry.rb
+++ app/models/note_entry.rb
@@ -2,4 +2,8 @@ class NoteEntry < ActiveRecord::Base
   belongs_to :supplier
   has_many :entry_subarticles
   accepts_nested_attributes_for :entry_subarticles
+
+  def supplier_name
+    supplier.present? ? supplier.name : ''
+  end
 end
